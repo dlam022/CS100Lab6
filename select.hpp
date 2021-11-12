933446fb1526f9cacb307091bd7012d6d53d7ee6
dlam022 <dericklam2@gmail.com>
2021-11-11 18:09:56 -0800

    Updated file

--- select.hpp
+++ select.hpp
@@ -2,6 +2,7 @@
 #define __SELECT_HPP__
 
 #include <cstring>
+#include <iostream>
 
 class Select
 {
@@ -24,7 +25,7 @@ class Select_Column: public Select
 protected:
     int column;
 public:
-    Select_Column(const Spreadsheet* sheet, const std::string& name)
+    Select_Column(const Spreadsheet* sheet, const string& name)
     {
         column = sheet->get_column_by_name(name);
     }
@@ -35,7 +36,96 @@ public:
     }
 
     // Derived classes can instead implement this simpler interface.
-    virtual bool select(const std::string& s) const = 0;
+    virtual bool select(const string& s) const = 0;
 };
+
+class Select_And : public Select { //the row contains this AND this
+	private:
+		Select* val1;
+		Select* val2;
+	public:
+		Select_And(Select* select1, Select* select2) {
+			val1 = select1; //this will get Select_Contains(&sheet, "Last", "Dole")
+			val2 = select2; //this will get Select_Not( Select_Contains(&sheets, "First", "V")
+		}
+		~Select_And() {
+			delete val1;
+			delete val2;
+		}
+
+		virtual bool select(const Spreadsheet* sheet, int row) const {
+			if(val1->select(sheet,row) && val2->select(sheet,row)) { //returns true if the row contain both the select_contains
+				return true;
+			}
+			return false;
+		}
+};
+
+class Select_Or: public Select { //the row contains this OR this
+	private:
+		Select* val3;
+		Select* val4;
+	public:
+		Select_Or(Select* select1, Select* select2) {
+			val3 = select1; //this get Select_Contains(&sheet, "First", "Amanda")
+			val4 = select2; //this get Select_Contains(&sheet, "Last", "on") or Select_Contains(&sheet,"Age", "9"))
+		}
+
+		~Select_Or{} {
+			delete val3;
+			delete val4;
+		}
+
+		virtual bool select(const Spreadsheet* sheet, int row) const {
+			if(val3->select(sheet,row) || val4->select(sheet,row)) { //if the row has this OR this, return true
+				return true;
+			}
+			return false;
+		}
+};
+
+class Select_Not : public Select {
+	private: 
+		Select* val5;
+	public:
+		Select_Not(Select* select1) {
+			val5 = select1;
+		}
+		~Select_Not{} {
+			delete val5;
+		}
+
+		virtual bool select(const Spreadsheet* sheet, int row) const {
+			bool temp = this->val5->select(sheet,row); //if the row does not contain this string, return true
+			if(!temp) {
+				return true;
+			}
+			return false;
+		}
+};
+
+class Select_Contains : public Select {
+	protected: 
+		const Spreadsheet* s;
+		int column
+		string val;
+	public:
+		Select_Contains(const Spreadsheet* sheet, const string& columnWanted, const string& stringWanted) { //example Select_Contains(&sheets, "First" (columnWanted), "Amanda" (stringWanted)
+			column = sheet->get_column_by_name(columnWanted); //column = getcolumn name from the spreadsheet 
+			s = sheet; 
+			val = stringWanted; 
+		}
+
+		virtual bool select(const Spreadsheet* sheet, int row) const {
+			return select(row);
+		}
+
+		virtual bool select(int row) const {
+			if(s->cell_data(row,column).find(val) != string::npos) { //use cell data to get data at the row number and column number. Find string in that row and column. Use string::npos to check if the string val matches the one in the row and column by checking each letter in the string
+				return true;
+			}
+		}
+};		
+		
 
 #endif //__SELECT_HPP__
